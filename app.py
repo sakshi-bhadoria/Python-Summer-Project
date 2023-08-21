fb986cee37071a77eca5da5e7744e02b43ba0bf7
sakshi-bhadoria <106387582+sakshi-bhadoria@users.noreply.github.com>
2023-08-21 17:05:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,29 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Handle form submission data
+        # Handle form submission data Food Provider
         name = request.form.get('name')
         email = request.form.get('email')
-        message = request.form.get('message')
+        phone = request.form.get('phone')
+        address = request.form.get('address')
+        
+        # Handle form submission data for the food
+        quantity = request.form.get('quantity')
+
+        # Handle volunteer data 
+        namev = request.form.get('name of volunteer')
+        emailv = request.form.get('email of the volunteer')
+        phonev = request.form.get('phone no of the volunteer')
+        
+        
         # You can process the form data here
         
-        # For demonstration, just printing the data
-        print(f"Name: {name}\nEmail: {email}\nMessage: {message}")
+        # For demonstration, just printing the data in the console that the client will be providing
+        print(f"Name: {name}\nEmail: {email}\nPhone: {phone}\nAddress: {address}\nQuantity: {quantity}")
+
+        # For demonstration, just printing the data in the console that the volunteer will be providing
+        print(f"Name: {namev}\nEmail: {emailv}\nPhone: {phonev}")
+        
     
     return render_template('form.html')
 
